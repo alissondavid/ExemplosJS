342c18dbe2a537c9f0b18b2fde374b63276c8a5d
Alisson David <alissondavidsouza@hotmail.com>
2023-12-15 09:42:29 -0300

    Exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,15 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica);
+
+var a = 10
+var b = 0
+
+console.log(a / b) // Infinity
